cc1a0c666ccfb478ff57eb7655472eae2430fb80
Bayoalliy <adebayoalliy@gmail.com>
2022-09-08 15:26:26 -0700

    ff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,7 @@
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	fwrite(2, str1, 59);
+	fprintf(stderr, "and that piece of art is useful\" -
+Dora Korpar, 2015-10-19");
 	return (1);
 }
