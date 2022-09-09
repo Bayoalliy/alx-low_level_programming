10ec7cc524178c2c5d403b9e804bc107389ba571
Bayoalliy <adebayoalliy@gmail.com>
2022-09-09 08:35:22 -0700

    using break and continue

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+			continue;
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
