ccf06dac539c70fdf5e10a8bb1d06dade35ff680
Bayoalliy <adebayoalliy@gmail.com>
2022-09-09 09:08:06 -0700

    printing hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	char ch;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
