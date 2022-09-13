9f2409d600839514aca70280d34c892feba95d7e
Bayoalliy <adebayoalliy@gmail.com>
2022-09-13 08:05:54 -0700

    returnin and putting

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_sign- check if parameter c is positive , negative or zero
+ *
+ * Return: On success 1
+ *
+ * @n: is an integer
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+
+}
+
