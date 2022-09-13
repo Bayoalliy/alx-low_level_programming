0386ea0501d86436100c8a6993b1e812f5d5315d
Bayoalliy <adebayoalliy@gmail.com>
2022-09-13 04:36:50 -0700

    printing alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *
+ * main - checks the code
+ *
+ * Return: On success 1.
+ *
+ */
+
+int _putchar(char c)
+{
+	print_alphabet();
+	return 0;
+}
