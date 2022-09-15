38d9b5a621977c545860223d258483c4a7ed43a2
Bayoalliy <adebayoalliy@gmail.com>
2022-09-15 07:13:38 -0700

    printing lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_line - prints numbers from 0 - 9 except 2 and 4
+ *
+ *@n : number of _ to print
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n > 0)
+			_putchar(95);
+	}
+	_putchar('\n');
+}
