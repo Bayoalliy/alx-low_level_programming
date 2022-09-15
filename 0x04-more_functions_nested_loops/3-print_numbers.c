f1413f0a814a6ff89696d99246543a0879cc328f
Bayoalliy <adebayoalliy@gmail.com>
2022-09-15 06:10:17 -0700

    print 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_numbers - prints numbers from 0 - 9
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
