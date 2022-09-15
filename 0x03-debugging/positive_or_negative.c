ce16e3bd528c2ad410f06830f718ce0cc6ed8df5
Bayoalliy <adebayoalliy@gmail.com>
2022-09-14 23:33:23 -0700

    second commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+void positive_or_negative(int n)
+{
+	/* your code goes there */
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+}
