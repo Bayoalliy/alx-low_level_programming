698d5ecbcbe14c00d7bd6d7e97d13c4ef1f21915
Bayoalliy <adebayoalliy@gmail.com>
2022-09-15 00:01:16 -0700

    hmm

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -12,4 +12,8 @@ void positive_or_negative(int i)
 	/* your code goes there */
 	if (i == 0)
 		printf("%d is zero\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
 }
