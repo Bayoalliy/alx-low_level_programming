e1ee337528d48d6d8e88a3b0983501819ddd5578
Bayoalliy <adebayoalliy@gmail.com>
2022-09-18 05:27:31 -0700

    used printf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,105 +1,38 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
  * print_to_98 - Prints all natural numbers
  * greater - print from numbers bigger than 98
  *
- * lesser - print from numbers lesser than 98.
- *
- * @n: starting point of count
+ * lesser - print from numb * @n: starting point of count
  *
  * print_to_98 - main function
  *
  **/
-void greater(int n)
-{
-	if (n > 98)
-	{
-		int i;
-
-		for (i = n; i >= 98; i--)
-		{
-			if (i >= 100)
-			{
-				int last = i % 100;
-
-				int mid = last / 10;
-
-				_putchar((i / 100) + '0');
-				_putchar(mid + '0');
-				_putchar((i % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-		}
-		_putchar('\n');
-	}
-}
-
-void lesser(int n)
-{
-		int i;
-
-		for (i = n; i <= 98; i++)
-			if (i < 10)
-			{
-				if (i < 0)
-				{
-					if (i < -9)
-					{
-						_putchar('-');
-						_putchar((abs(i) / 10) + '0');
-						_putchar((abs(i) % 10) + '0');
-					}
-					else
-					{
-						_putchar('-');
-						_putchar(abs(i) + '0');
-					}
-				}
-				else
-				{
-					_putchar(i + '0');
-				}
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-		_putchar('\n');
-
-}
-
-
 void print_to_98(int n)
 {
-	if (n > 98)
-		greater(n);
-	else if (n < 98)
-		lesser(n);
-	else if (n == 98)
+	int i;
+
+	if (n <= 98)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		_putchar('\n');
+
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d",i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
 	}
 }
