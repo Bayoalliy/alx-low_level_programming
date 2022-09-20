cc4b69bca51c025e9ac09f7d2a314bb353d73732
Bayoalliy <adebayoalliy@gmail.com>
2022-09-20 15:18:36 -0700

    swaping ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+/**
+ * swap_int - swap the value of two ints
+ *
+ * @a : pointer to the variable a
+ * @b : pointer to the variable b
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
