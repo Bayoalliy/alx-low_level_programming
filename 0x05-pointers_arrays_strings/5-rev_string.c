0272026f73c988c3d51a0d22c7f965283e33cc2a
Bayoalliy <adebayoalliy@gmail.com>
2022-09-20 20:32:30 -0700

    printing every other character

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * rev_string - prints the reverse of a string
+ * print_rev - prints the reverse of a string
  *
  * @s : pointer to the variable str
  */
 void rev_string(char *s)
 {
-	int a, count = 0;
+	int a, i = 0, count = 0;
+
+	char *rev = s;
 
 
-	while (*(s + count) != '\0')
+	while (s[count] != '\0')
 		count++;
 
-	a = count;
 
 	for (a = count - 1; a >= 0; a--)
 	{
-		putchar(*(s + a));
+		rev[i] = s[a];
+		i++;
 	}
-	putchar('\n');
+
+	rev[a] = '\0';
 }
