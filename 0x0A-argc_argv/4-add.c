275a51bd34862bf528905f8711c5c920d350ee98
Bayoalliy <adebayoalliy@gmail.com>
2022-09-30 13:31:04 -0700

    adding argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point.
+ * @argc : argument count
+ * @argv : argument vector
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; j < 1; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
