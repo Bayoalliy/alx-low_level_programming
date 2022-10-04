d9e2e1b0121415e5028e72f9e4b96520c3520972
Bayoalliy <adebayoalliy@gmail.com>
2022-10-04 16:48:19 -0700

    ff

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		a[i] = malloc(sizeof(int |) * width);
+		a[i] = malloc(sizeof(int) * width);
 		if (a == NULL)
 		{
 			for (i--; i <= 0; i--)
