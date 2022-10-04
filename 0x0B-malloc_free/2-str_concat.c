db5f5f5378e056096179a52c7a75fce52fdfc93b
Bayoalliy <adebayoalliy@gmail.com>
2022-10-04 12:29:49 -0700

    concatenating

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 	a = malloc((strlen(s1) + strlen(s2)) + 1);
 
 	if (a == NULL)
