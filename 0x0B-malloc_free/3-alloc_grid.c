d1b5c3ec4b5b7fb044eb4d230c32814f2c73962b
Bayoalliy <adebayoalliy@gmail.com>
2022-10-04 17:07:04 -0700

    final

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 		a[i] = malloc(sizeof(int) * width);
 		if (a[i] == NULL)
 		{
-			for (i--; i <= 0; i--)
+			for (i--; i >= 0; i--)
 				free(a[i]);
 			free(a);
 			return (NULL);
