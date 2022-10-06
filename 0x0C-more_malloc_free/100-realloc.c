99581c1c7ad9c8fb75d8919e9ae4db1e6041a9d9
Bayoalliy <adebayoalliy@gmail.com>
2022-10-06 09:48:49 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,11 +23,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
-		if (ptr == NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
