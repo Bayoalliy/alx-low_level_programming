d1518e60735a5ddba90d8847416e779e88f2a80d
Bayoalliy <adebayoalliy@gmail.com>
2022-10-10 10:17:27 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog.
+ * @owner: name of owner.
+ */
+
+
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+};
+#endif
