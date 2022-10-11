a8c9f7302ab6346e33254f4cd86df75b6d5955f3
Bayoalliy <adebayoalliy@gmail.com>
2022-10-11 04:46:32 -0700

    free

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,11 +19,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_d = malloc(sizeof(*new_d));
 
 	if (new_d == NULL || name == NULL || owner == NULL)
+	{
+		free(new_d);
 		return (NULL);
-
+	}
 	new_d->name = malloc(strlen(name) + 1);
-	new_d->owner = malloc(strlen(owner) + 1);
 
+	new_d->owner = malloc(strlen(owner) + 1);
+	if ((new_d->owner) == NULL || (new_d->name) == NULL)
+	{
+		free(new_d->owner);
+		free(new_d->name);
+		free(new_d);
+		return (NULL);
+	}
 	for (i = 0; i < strlen(name) + 1; i++)
 	{
 		new_d->name[i] = name[i];
@@ -33,5 +42,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		new_d->owner[i] = owner[i];
 
 	new_d->age = age;
+
 	return (new_d);
 }
