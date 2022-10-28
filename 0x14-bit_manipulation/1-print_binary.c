be90beeb0a6b1ba0e378175e339fcbdc593f44a6
Bayoalliy <adebayoalliy@gmail.com>
2022-10-28 11:01:41 -0700

    binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,22 +8,15 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 1;
-
-	if (n == 0)
-		_putchar('0');
-	else if (n == 1)
-		_putchar('1');
-	else
+	if (n >> 1)
 	{
-		while (i * 2 <= n)
-			i *= 2;
-		for (; i > 0; i >>= 1)
-		{
-			if ((n & i) == 0)
-				_putchar('0');
-			else
-				_putchar('1');
-		}
+		print_binary(n >> 1);
+		if (n & 1)
+			_putchar('1');
+		else
+			_putchar('0');
 	}
+	else
+		_putchar(n + '0');
 }
+
