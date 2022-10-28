07cef108d25f611d5ba53c4051ec2dce82e47136
Bayoalliy <adebayoalliy@gmail.com>
2022-10-28 06:58:54 -0700

    converter

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - convert decimal to binary.
+ * @n: decimal to be converted.
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int i = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else if (n == 1)
+		_putchar('1');
+	else
+	{
+		while (i * 2 <= n)
+			i *= 2;
+		for (; i > 0; i >>= 1)
+		{
+			if ((n & i) == 0)
+				_putchar('0');
+			else
+				_putchar('1');
+		}
+	}
+}
