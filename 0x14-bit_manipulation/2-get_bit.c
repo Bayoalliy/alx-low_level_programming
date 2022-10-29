afe5ddf28f8aeb036934bbe56d34373d69149f5a
Bayoalliy <adebayoalliy@gmail.com>
2022-10-28 23:17:35 -0700

    index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,19 +9,23 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n >> 1)
+	unsigned int i = 1;
+
+	int ind = index;
+
+	if (n == 0 && ind == 0)
+		return (0);
+
+	for (; i <= n; i *= 2, ind--)
 	{
-		if (index == 0)
+		if (ind == 0)
 		{
-			if (n & 1)
+			if (n & i)
 				return (1);
 			else
 				return (0);
 		}
-		get_bit((n >> 1), --index);
 	}
-	if (index > 0)
-		return (-1);
-	return (1);
+	return (-1);
 }
 
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -14,7 +14,7 @@ int main(void)
     printf("%d\n", n);
     n = get_bit(98, 1);
     printf("%d\n", n);
-    n = get_bit(2, 2);
+    n = get_bit(1, 1);
     printf("%d\n", n);
     return (0);
 }
