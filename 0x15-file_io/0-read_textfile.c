8f40de02d499d172b104f35b5bc7457d674cbc71
Bayoalliy <adebayoalliy@gmail.com>
2022-10-31 17:08:40 -0700

    Tread lightly, shte is bear

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - read a text file and print it to the standard output.
+ * @filename: file to be read from.
+ * @letters: numbers of letters it should read and print.
+ * Return: the actual number of letters it could print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd;
+
+	char *c = malloc(letters * sizeof(char));
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_EXCL | O_RDONLY, 0700);
+	if (fd == -1)
+		return (0);
+
+	rd = read(fd, c, letters);
+	if (rd < 0)
+		return (0);
+
+	close(fd);
+
+	printf("%s", c);
+	return (rd);
+}
+
+
+
+
