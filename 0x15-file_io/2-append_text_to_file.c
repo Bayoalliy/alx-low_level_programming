5255343d670d860c5ee4d079000b3a0658a7e3b8
Bayoalliy <adebayoalliy@gmail.com>
2022-11-01 16:05:26 -0700

    ff

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,9 +21,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_APPEND, 0600);
+	fd = open(filename, O_RDWR | O_APPEND, 0600);
 	if (fd < 0)
 		return (-1);
+
+	if (!text_content)
+		return (1);
 
 	w = write(fd, text_content, strlen(text_content));
 	if (w < 0)
