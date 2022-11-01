bda194769efcd1752a0b7c1a21b145795677a6ca
Bayoalliy <adebayoalliy@gmail.com>
2022-10-31 19:23:53 -0700

    crate

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,33 +13,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, rd;
-
-	int sz;
-
-	char *c = malloc((strlen(text_content) + 1) * sizeof(char));
+	int o, w, len = 0;
 
 	if (filename == NULL)
-		return (0);
-	fd = open(filename, O_CREAT | O_RDWR, 0700);
-	if (fd == -1)
-		return (0);
+		return (-1);
 
-	sz = write(fd, text_content, strlen(text_content) + 1);
-	if (sz < 0)
-		return (0);
-	rd = read(fd, c, strlen(text_content) + 1);
-	if (rd < 0)
-		return (0);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	sz = write(STDOUT_FILENO, c, rd);
-	if (sz < 0)
-		return (0);
-	close(fd);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
 
 	return (1);
 }
-
 
 
 
