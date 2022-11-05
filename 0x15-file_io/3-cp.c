252b1c186f67bbd037a337aae72b956188fe9592
Bayoalliy <adebayoalliy@gmail.com>
2022-11-04 17:43:09 -0700

    stderr

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,14 +7,14 @@
  * @file_name: name of affected file.
  */
 
-void error_check(int fd1, int fd2, char *file_name)
+void error_check(int fd, char *file_name)
 {
-	if (fd1 == -1)
+	if (fd == 3)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
 		exit(98);
 	}
-	if (fd2 == -1)
+	else
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
 		exit(99);
@@ -37,19 +37,19 @@ void copy(char *file_from, char *file_to)
 
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 < 0)
-		error_check(fd1, 0, file_from);
+		error_check(fd1, file_from);
 	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	if (fd2 < 0)
-		error_check(0, fd2, file_to);
+		error_check(fd2, file_to);
 
 	do {
 		r = read(fd1, buf, 1024);
 		if (r < 0)
-			error_check(fd1, 1, file_from);
+			error_check(fd1, file_from);
 		w = write(fd2, buf, r);
 		if (w < 0)
-			error_check(1, fd2, file_to);
+			error_check(fd2, file_to);
 	} while (r == 1024);
 
 	if (close(fd1) < 0)
