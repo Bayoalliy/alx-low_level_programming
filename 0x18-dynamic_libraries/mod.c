b5add5c722ca61c06bbe0ab11510a23d9cdf3e24
Bayoalliy <adebayoalliy@gmail.com>
2022-12-12 10:17:54 -0800

    CPython

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Return: addition of two integers.
+ *
+ * @a:  first parameter of integer type
+ * @b:  parameters of integer type
+ */
+int mod(int a, int b)
+{
+	int added = a % b;
+
+	return (added);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/**
+ * sub - Entry point
+ *
+ * Return: addition of two integers.
+ *
+ * @a:  first parameter of integer type
+ * @b:  parameters of integer type
+ */
+int sub(int a, int b)
+{
+	int added = a - b;
+
+	return (added);
+}
