c11f1f901c87f191c2aed3da42a0bdddba17cc68
Bayoalliy <adebayoalliy@gmail.com>
2022-12-14 02:21:05 -0800

    preliad

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD=https://github.com/Bayoalliy/alx-low_level_programming/0x18-dynamic_libraries/lib.so
+wget -P https://github.com/Bayoalliy/alx-low_level_programming/0x18-dynamic_libraries/raw/master/lib.so
+export LD_PRELOAD=/tmp/lib.so
