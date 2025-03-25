981a5394caeb2c5ed7bcdec46d1d3edc61e87f4a
Bayo <adebayoalliy@gmail.com>
2025-03-25 03:48:45 -0700

    ff

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints an integer.
+ *
+ * @n : integer to be printed.
+ *
+ */
+
+
+void print_number(int n)
+{
+	int num, abs_num = n, len = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		abs_num *= -1;
+	}
+
+	num = abs_num;
+	while (num / 10)
+	{
+		len *= 10;
+		num /= 10;
+	}
+
+	while (len / 10)
+	{
+		_putchar((abs_num / len) + 48);
+		abs_num %= len;
+		len /= 10;
+	}
+	_putchar(abs_num + 48);
+}
