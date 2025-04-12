f1b62771051e6b0cae812399c46f6b01fc554ccb
Bayo <adebayoalliy@gmail.com>
2025-04-12 15:16:54 -0700

    final

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include <string.h>
 
 /**
  * print_all - prints anything.
@@ -17,7 +16,7 @@ void print_all(const char * const format, ...)
 	char *val;
 
 	va_start(args, format);
-	while (++i < (int)strlen(format))
+	while (format && format[++i])
 	{
 		if (format[i] != 'c' && format[i] != 'i'
 		&& format[i] != 'f' && format[i] != 's')
